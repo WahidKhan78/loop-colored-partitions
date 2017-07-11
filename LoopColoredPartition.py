4dc47fb65d4e1f04b46ecc37f03779e758bafe89
Isaac Garfinkle <garfinklei@carleton.edu>
2017-07-11 00:14:29 -0500

    added support for specifying part length

--- LoopColoredPartition.py
+++ LoopColoredPartition.py
@@ -36,12 +36,14 @@ def partitions_from_profile_max_size(profile, max_weight, kwargs):
         partitions.append([])
         return partitions
     num_colors = len(profile)
-    next_max = max_weight+1
+    next_max = max_weight + 1
     if kwargs.get('unique_parts', False):
         next_max = max_weight
     elif kwargs.get('unique_sizes', False):
-        next_max = (max_weight/num_colors)* num_colors
+        next_max = (max_weight / num_colors) * num_colors
     for weight in reversed(xrange(next_max)):
+        if 'length_list' in kwargs and (weight/num_colors)+1 not in kwargs['length_list']:
+            continue
         row = get_row_from_weight(weight, num_colors)
         this_row_profile = profile_of_row(row[0], num_colors, row[1])
         if dominates(profile, this_row_profile):
@@ -53,13 +55,22 @@ def partitions_from_profile_max_size(profile, max_weight, kwargs):
     return partitions
 
 
+# Options are thus:
+# unique_parts = True generates only partitions with distinct parts.
+# 12/21/2/1 is generated under this condition.
+# unique_sizes = True generates only partitions whose rows are distinct sizes.
+# 12/21/2/1 is not generated under this condition.
+# max_part = k generates partitions with largest part at most length k.
+# length_list = a[n] generates partitions all of whose lengths are in the list a.
 def partitions_from_profile(profile, **kwargs):
     lst = []
-    for p in partitions_from_profile_max_size(profile,
-                                              len(profile) * sum(profile),
-                                              kwargs):
+    for p in partitions_from_profile_max_size(
+            profile,
+            len(profile) * kwargs.get('max_part', sum(profile)),
+            kwargs):
         lst.append(loop_colored_partition(reversed(p), len(profile)))
     return lst
 
-for p in partitions_from_profile([5,5,5], unique_sizes = True):
+
+for p in partitions_from_profile([5, 5, 5], length_list = [1,3,5,7,9,11,13,15,17]):
     print p
