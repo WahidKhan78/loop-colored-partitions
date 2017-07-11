87fa70c568929c30d2db9746e429ff6aa592350a
Isaac Garfinkle <garfinklei@carleton.edu>
2017-07-11 01:35:15 -0500

    Can now generate partitions with fixed number of parts

--- LoopColoredPartition.py
+++ LoopColoredPartition.py
@@ -30,10 +30,14 @@ def dominates(l1, l2):
     return all(l1[i] >= l2[i] for i in xrange(len(l1)))
 
 
-def partitions_from_profile_max_size(profile, max_weight, kwargs):
+def partitions_from_profile_max_size(profile, max_weight, num_parts, kwargs):
     partitions = []
     if all(p == 0 for p in profile):
-        partitions.append([])
+        if 'num_parts_list' in kwargs:
+            if num_parts in kwargs['num_parts_list']:
+                partitions.append([])
+        else:
+            partitions.append([])
         return partitions
     num_colors = len(profile)
     next_max = max_weight + 1
@@ -49,7 +53,7 @@ def partitions_from_profile_max_size(profile, max_weight, kwargs):
         if dominates(profile, this_row_profile):
             remaining = [profile[i] - this_row_profile[i]
                          for i in xrange(len(profile))]
-            for partition in partitions_from_profile_max_size(remaining, weight, kwargs):
+            for partition in partitions_from_profile_max_size(remaining, weight, num_parts + 1, kwargs):
                 partition.append(row)
                 partitions.append(partition)
     return partitions
@@ -61,16 +65,14 @@ def partitions_from_profile_max_size(profile, max_weight, kwargs):
 # unique_sizes = True generates only partitions whose rows are distinct sizes.
 # 12/21/2/1 is not generated under this condition.
 # max_part = k generates partitions with largest part at most length k.
-# length_list = a[n] generates partitions all of whose lengths are in the list a.
+# length_list = lst generates partitions whose parts are all of lengths in lst.
+# num_parts_list = lst generates partitions whose number of parts is in lst.
 def partitions_from_profile(profile, **kwargs):
     lst = []
     for p in partitions_from_profile_max_size(
             profile,
             len(profile) * kwargs.get('max_part', sum(profile)),
+            0,
             kwargs):
         lst.append(loop_colored_partition(reversed(p), len(profile)))
     return lst
-
-
-for p in partitions_from_profile([5, 5, 5], length_list = [1,3,5,7,9,11,13,15,17]):
-    print p
---
+++ Profiles.py
@@ -0,0 +1,34 @@
+def get_profiles(num_colors, size):
+    if num_colors == 1:
+        return [[size]]
+    profiles = []
+    for i in xrange(size+1):
+        for prof in get_profiles(num_colors-1, size-i):
+            profiles.append(prof + [i])
+    return profiles
+
+def is_necklace(comp):
+    representative = int(''.join(map(str,list(comp))))
+    for i in xrange(len(comp)):
+        if (representative > int(''.join(map(str,list(comp)[i:]+list(comp)[:i])))):
+            return False
+    return True
+
+def is_bracelet(profile):
+    representative = int(''.join(map(str,list(profile))))
+    for i in xrange(len(profile)):
+        if (representative > int(''.join(map(str,list(profile)[i:]+list(profile)[:i])))):
+            return False
+    reverse = profile[::-1]
+    for i in xrange(len(profile)):
+        if (representative > int(''.join(map(str,list(reverse)[i:]+list(reverse)[:i])))):
+            return False
+    return True
+
+def get_necklaces(num_colors,size):
+    return [x for x in get_profiles(num_colors,size) if is_necklace(x)]
+
+def get_bracelets(num_colors, size):
+    return [x for x in get_profiles(num_colors,size) if is_bracelet(x)]
+
+print get_bracelets(3,5)
