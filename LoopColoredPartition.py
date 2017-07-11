5a695729818bce4e410de211551585b05de7f858
Isaac Garfinkle <garfinklei@carleton.edu>
2017-07-10 23:06:28 -0500

    Adding options to generator: distinct parts and distinct sizes

--- LoopColoredPartition.py
+++ LoopColoredPartition.py
@@ -30,26 +30,36 @@ def dominates(l1, l2):
     return all(l1[i] >= l2[i] for i in xrange(len(l1)))
 
 
-def partitions_from_profile_max_size(profile, max_weight, unique_parts = False):
+def partitions_from_profile_max_size(profile, max_weight, kwargs):
     partitions = []
     if all(p == 0 for p in profile):
         partitions.append([])
         return partitions
     num_colors = len(profile)
-    for weight in reversed(xrange(max_weight if unique_parts else max_weight + 1)):
+    next_max = max_weight+1
+    if kwargs.get('unique_parts', False):
+        next_max = max_weight
+    elif kwargs.get('unique_sizes', False):
+        next_max = (max_weight/num_colors)* num_colors
+    for weight in reversed(xrange(next_max)):
         row = get_row_from_weight(weight, num_colors)
         this_row_profile = profile_of_row(row[0], num_colors, row[1])
         if dominates(profile, this_row_profile):
             remaining = [profile[i] - this_row_profile[i]
                          for i in xrange(len(profile))]
-            for partition in partitions_from_profile_max_size(remaining, weight):
+            for partition in partitions_from_profile_max_size(remaining, weight, kwargs):
                 partition.append(row)
                 partitions.append(partition)
     return partitions
 
 
-def partitions_from_profile(profile):
+def partitions_from_profile(profile, **kwargs):
     lst = []
-    for p in partitions_from_profile_max_size(profile, len(profile) * sum(profile)):
+    for p in partitions_from_profile_max_size(profile,
+                                              len(profile) * sum(profile),
+                                              kwargs):
         lst.append(loop_colored_partition(reversed(p), len(profile)))
     return lst
+
+for p in partitions_from_profile([5,5,5], unique_sizes = True):
+    print p
